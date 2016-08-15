cc6d4246ee1eb823fb5d124d436621cf6ed5f2ad
ainsleys <sutherlande@gmail.com>
2016-08-15 14:01:27 -0700

    About to integrate with the webvr boilerplate

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 var express = require('express');
 var app = express();
 
-//var app = require('express')();
+
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
-//var ss = require('socket.io-stream');
+
 var path = require('path');
 var wav = require('wav');
 var ogg = require('ogg');
 var stringify = require('node-stringify');
 var multer  = require('multer')
-//var opus = require('opus-recorder');
+
 
 var counter = 0;
 var streamcounter = 0;
 var streamOutfile = 'streamdemo.wav';
-//var practice = 'practice.wav'
+
 var opus = require('node-opus');
 var oggtrack = '/public/recordings/track.ogg';
 var fileName = new Date().toISOString() + ".ogg";
-//var FormData = require('form-data');
 
-
-//var upload = multer({ dest: 'uploads/'})
 
 
 
@@ -44,15 +41,10 @@ var practice = './public/testpractice.ogg';
 io.on('connection', function(socket){
   	console.log('a user connected');
 
-  	//var form = new FormData();
-
-	  	
 
   	socket.on('audioURL', function(dataBlob){
 
-  		//var dataBlob = dataBlob
   		var test = stringify(dataBlob);
-  		//var practice = '/public/recordings/practice.ogg'
 
 
  		console.log("received audio blubb at the server!")
